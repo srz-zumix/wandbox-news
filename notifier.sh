c04a55187c90ab8295569820432cc36310f433c0
srz_zumix <zumix.cpp@gmail.com>
2017-04-28 01:43:20 +0900

    remove

--- notifier.sh
+++ notifier.sh
@@ -2,5 +2,4 @@
 
 LISTFILE=wandbox-compilers.txt
 python wandbox-listup-compiler.py > $LISTFILE
-#curl https://circleci.com/api/v1.1/project/github/circleci/wandbox-news/$CIRCLE_BUILD_NUM/artifacts/0/$CIRCLE_ARTIFACTS/$LISTFILE
 
