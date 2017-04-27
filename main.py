36945560777ae9c3c3bbc08a376ab4e608c2e0bd
srz_zumix <zumix.cpp@gmail.com>
2017-04-28 01:31:02 +0900

    update

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+from google.appengine.api import app_identity
+from googleapiclient import discovery
+from oauth2client.client import GoogleCredentials
+
+compute = discovery.build(
+    'compute', 'v1', credentials=GoogleCredentials.get_application_default())
+
+def get_project_metadata(metadata_key):
+    project_id = app_identity.get_application_id()
+    project = compute.projects().get(project=project_id).execute()
+    for entry in project['commonInstanceMetadata']['items']:
+        if entry['key'] == metadata_key:
+            return entry['value']
+    return None
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
-
 
 @app.route('/')
 def hello():
@@ -13,12 +27,17 @@ def hello():
     return 'Hello World!'
 
 
+@app.route('/secret')
+def secret():
+    return get_project_metadata('my_key')
+
+
 @app.route('/wandbox-news-kick')
 def kick():
     """Return a friendly HTTP greeting."""
     project = ''
     branch = 'master'
-    token = ''
+    token = get_project_metadata('circle_ci_token')
     trigger_build_url='https://circleci.com/api/v1.1/project/github/{0}/tree/{1}'.format(project, branch)
     if token:
         trigger_build_url += '?circle-token=' + token
